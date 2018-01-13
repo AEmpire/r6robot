bc0c5e28805e654656b910be18b80350c9ebdbda
aempire <omgshili@gmail.com>
2018-01-14 02:17:52 +0800

    login finish

--- login/login.go
+++ login/login.go
@@ -1,30 +1,104 @@
 package login
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"regexp"
 	"time"
 )
 
+//vfRet 转换接收vfwebqq的json
+type vfRet struct {
+	Retcode int
+	Result  struct {
+		Vfwebqq string
+	}
+}
+
+//uinPost 获取uin与psessionid的POST内容
+type uinPost struct {
+	Ptwebqq    string `json:"ptwebqq"`
+	Clientid   int    `json:"clientid"`
+	Psessionid string `json:"psessionid"`
+	Status     string `json:"status"`
+}
+
+//uinJSON 包含uin与psessionid的json
+type uinRet struct {
+	Result struct {
+		Cip        int    `json:"cip"`
+		F          int    `json:"f"`
+		Index      int    `json:"index"`
+		Port       int    `json:"port"`
+		Psessionid string `json:"psessionid"`
+		Status     string `json:"status"`
+		Uin        int    `json:"uin"`
+		UserState  int    `json:"user_state"`
+		Vfwebqq    string `json:"vfwebqq"`
+	} `json:"result"`
+	Retcode int `json:"retcode"`
+}
+
+//Gets 包含通过登录获得的用于轮询消息以及其他用途的重要参数
+type Gets struct {
+	//获取好友信息
+	Vfwebqq string
+	//发送消息
+	Psessionid string
+	//发送消息
+	Uin int
+	//重要cookies
+	Cookies []*http.Cookie
+}
+
+//AddCookie 添加cookie
+func (g Gets) AddCookie(cookie *http.Cookie) {
+	g.Cookies = append(g.Cookies, cookie)
+}
+
+const (
+	//urlVfqq 获取vfwebqq的固定url
+	urlVfqq = `https://s.web2.qq.com/api/getvfwebqq?ptwebqq=&clientid=53999199&psessionid=&t=1515849548201`
+	//urlUin 获取uin以及psessionid的固定url
+	urlUin = `https://d1.web2.qq.com/channel/login2`
+)
+
 //Login 登录QQ
-func Login() error {
+func Login() (Gets, error) {
+	uinBody := uinPost{
+		Clientid:   53999199,
+		Status:     "online",
+		Ptwebqq:    "",
+		Psessionid: ""}
+
+	var loginGets Gets
+	//提取二维码状态码
 	regexStat := regexp.MustCompile(`ptuiCB\(\'(\d+)\'`)
+
+	//判断二维码是否已扫描
 	isScanned := false
+
+	//保存扫描二维码后返回的url
 	var sigLink string
 
+	//获取二维码
 	for !isScanned {
-		qrsig, errCode := GetQcode()
-		if errCode != nil {
-			return errCode
+		qrsig, err := GetQcode()
+		if err != nil {
+			return loginGets, err
 		}
 	check_state:
 		for !isScanned {
-			resp, errStat := GetQRStat(qrsig)
-			if errStat != nil {
-				return errStat
+			resp, err := GetQRStat(qrsig)
+			if err != nil {
+				return loginGets, err
 			}
-			switch code := regexStat.FindAllStringSubmatch(resp, -1)[0][1]; code {
+			switch code := regexStat.FindAllStringSubmatch(resp.Status, -1)[0][1]; code {
 			case `65`:
+				//二维码失效重新获取
 				fmt.Println("二维码已失效")
 				break check_state
 			case `66`:
@@ -33,12 +107,72 @@ func Login() error {
 				fmt.Println("二维码认证中")
 			case `0`:
 				fmt.Println("二维码认证成功")
-				sigLink = regexp.MustCompile(`ptuiCB\(\'0\',\'0\',\'([^\']+)\'`).FindAllStringSubmatch(resp, -1)[0][1]
+				//通过正则获取返回的url
+				sigLink = regexp.MustCompile(`ptuiCB\(\'0\',\'0\',\'([^\']+)\'`).FindAllStringSubmatch(resp.Status, -1)[0][1]
 				fmt.Println(sigLink)
+				loginGets.Cookies = resp.Cookies
 				isScanned = true
 			}
 			time.Sleep(3 * time.Second)
 		}
 	}
-	return nil
+
+	//通过返回的url获取cookies
+	fmt.Println("cookies")
+	reqSig, err := http.Get(sigLink)
+	if err != nil {
+		return loginGets, err
+	}
+	println(reqSig.StatusCode)
+	
+	//获取vfwebqq
+	fmt.Println("vfwebqq")
+	reqVf, err := http.NewRequest("Get", urlVfqq, nil)
+	if err != nil {
+		return loginGets, err
+	}
+	for _, cookie := range loginGets.Cookies {
+		reqVf.AddCookie(cookie)
+	}
+	reqVf.Header.Set("Referer", "https://s.web2.qq.com/proxy.html?v=20130916001&callback=1&id=1")
+	client := &http.Client{}
+	respVf, err := client.Do(reqVf)
+	if err != nil {
+		return loginGets, err
+	}
+	defer respVf.Body.Close()
+	vfJSON, _ := ioutil.ReadAll(respVf.Body)
+	var vfret vfRet
+	err = json.Unmarshal([]byte(vfJSON), &vfret)
+	if err != nil {
+		return loginGets, err
+	}
+	loginGets.Vfwebqq = vfret.Result.Vfwebqq
+
+	//获取uin与psessionid
+	fmt.Println("uin")
+	uinpost, err := json.Marshal(uinBody)
+	reqUin, err := http.NewRequest("POST", urlUin, bytes.NewBuffer(uinpost))
+	if err != nil {
+		return loginGets, err
+	}
+	for _, cookie := range loginGets.Cookies {
+		reqUin.AddCookie(cookie)
+	}
+	reqUin.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	respUin, err := client.Do(reqUin)
+	if err != nil {
+		return loginGets, err
+	}
+	defer respUin.Body.Close()
+	uinJSON, _ := ioutil.ReadAll(respUin.Body)
+	var uinret uinRet
+	err = json.Unmarshal([]byte(uinJSON), &uinret)
+	if err != nil {
+		return loginGets, err
+	}
+	loginGets.Psessionid = uinret.Result.Psessionid
+	loginGets.Uin = uinret.Result.Uin
+
+	return loginGets, nil
 }
--- login/qrcode.go
+++ login/qrcode.go
@@ -7,6 +7,12 @@ import (
 	"time"
 )
 
+//ScannedSetting 扫描二维码后返回的设置参数
+type ScannedSetting struct{
+	Status string
+	Cookies []*http.Cookie
+}
+
 //hash33 自定哈希函数
 func hash33(qrsig string) int {
 	e := 0
@@ -18,52 +24,53 @@ func hash33(qrsig string) int {
 
 //GetQcode 获取登录二维码
 func GetQcode() (string, error) {
-	resp, errResp := http.Get("https://ssl.ptlogin2.qq.com/ptqrshow?appid=501004106")
+	resp, err := http.Get("https://ssl.ptlogin2.qq.com/ptqrshow?appid=501004106")
 
-	if errResp != nil {
-		return "", errResp
+	if err != nil {
+		return "", err
 	}
 	defer resp.Body.Close()
-	body, errRead := ioutil.ReadAll(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
 	cookie := resp.Cookies()
 	qrsig := cookie[0].Value
-	if errRead != nil {
-		return "", errRead
+	if err!= nil {
+		return "", err
 	}
 
 	filename := "/home/shili/图片/QCode.png"
-	errFileWrite := ioutil.WriteFile(filename, body, 0644)
-	if errFileWrite != nil {
-		return "", errFileWrite
+	err = ioutil.WriteFile(filename, body, 0644)
+	if err != nil {
+		return "", err
 	}
 	return qrsig, nil
 }
 
 //GetQRStat 获取QR码扫描状态
 //此处只需reqURL与cookie正确即可，无需referer
-func GetQRStat(qrsig string) (string, error) {
+func GetQRStat(qrsig string) (settings ScannedSetting, err error) {
 
 	qrtoken := strconv.Itoa(hash33(qrsig))
 	referer := "https://xui.ptlogin2.qq.com/cgi-bin/xlogin?daid=164&target=self&style=40&pt_disable_pwd=1&mibao_css=m_webqq&appid=501004106&enable_qlogin=0&no_verifyimg=1&s_url=https%3A%2F%2Fw.qq.com%2Fproxy.html&f_url=loginerroralert&strong_login=1&login_state=10&t=20131024001"
 	reqURL := "https://ssl.ptlogin2.qq.com/ptqrlogin?u1=https%3A%2F%2Fw.qq.com%2Fproxy.html&ptqrtoken=" + qrtoken + "&ptredirect=0&h=1&t=1&g=1&from_ui=1&ptlang=2052&action=0-0-1515480825809&js_ver=10233&js_type=1&login_sig=&pt_uistyle=40&aid=501004106&daid=164&mibao_css=m_webqq&"
 
-	req, errReq := http.NewRequest("GET", reqURL, nil)
-	if errReq != nil {
-		return "0", errReq
+	req, err := http.NewRequest("GET", reqURL, nil)
+	if err != nil {
+		return settings, err
 	}
 
 	req.Header.Set("Referer", referer)
 	cookie := http.Cookie{Name: "qrsig", Value: qrsig, Expires: time.Now().Add(30 * 24 * time.Hour)}
 	req.AddCookie(&cookie)
 	client := &http.Client{}
-	resp, errResp := client.Do(req)
-	if errResp != nil {
-		return "0", errResp
+	resp, err := client.Do(req)
+	if err != nil {
+		return settings, err
 	}
 
 	defer resp.Body.Close()
 	body, _ := ioutil.ReadAll(resp.Body)
-	respStr := string(body)
+	settings.Status = string(body)
+	settings.Cookies = resp.Cookies()
 
-	return respStr, nil
+	return settings, err
 }
